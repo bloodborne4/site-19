7a685a4ece418f4d87bcbc30517a76dfe051bbf3
bloodborne4 <80268997+bloodborne4@users.noreply.github.com>
2021-05-16 22:11:33 +0800

    Delete js directory

--- js/first.js
+++
@@ -1,9 +0,0 @@
-
-var colors = []; // 创建一个数组
-var count = colors.push('red', 'green'); // 末尾推入两项
-console.log(count); //2
-colors.push('black'); // 末尾推入另一项
-console.log(colors); //3
-var item = colors.pop(); // 末尾弹出最后一项
-console.log(item); //'black'
-console.log(colors); //2
